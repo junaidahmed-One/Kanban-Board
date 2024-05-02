cf2c59a2c31ec1531f870bdc6f83d30135659a9a
junaidahmed-One <junaidahmed882000@gmail.com>
2024-05-02 14:11:50 +0530

    structuring as columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import "./App.css";
 import { TaskCard } from "./Components/TaskCard";
-import { tasks } from "./Types/data-tasks";
+import { tasks, statuses } from "./Types/data-tasks";
 
 function App() {
+	const columns = statuses.map((status) => {
+		const tasksInColumn = tasks.filter((task) => task.status === status);
+		return {
+			title: status,
+			tasks: tasksInColumn,
+		};
+	});
+
 	return (
-		<>
-			{tasks.map((task) => (
-				<TaskCard task={task} />
+		<div className="flex divide-y">
+			{columns.map((column) => (
+				<div>
+					<h1>{column.title}</h1>
+					{column.tasks.map((task) => (
+						<TaskCard task={task} />
+					))}
+				</div>
 			))}
-		</>
+		</div>
 	);
 }
 
--- src/Types/data-tasks.ts
+++ src/Types/data-tasks.ts
@@ -1,28 +1,67 @@
+export type Status = "todo" | "in-progress" | "done";
+
 export type Task = {
 	title: string;
 	id: string;
+	status: Status;
 	points?: number;
 };
+
+export const statuses: Status[] = ["todo", "in-progress", "done"];
 
 export const tasks: Task[] = [
 	{
 		title: "Hello World",
 		id: "BUS-1",
+		status: "todo",
 		points: 5,
 	},
 	{
 		title: "Competitive Analysis",
 		id: "BUS-2",
+		status: "in-progress",
 		points: 10,
 	},
 	{
 		title: "Develop component",
 		id: "BUS-3",
+		status: "done",
 		points: 7,
 	},
 	{
 		title: "Test story",
 		id: "BUS-4",
+		status: "in-progress",
 		points: 20,
+	},
+	{
+		title: "Identify potential customers",
+		id: "BUS-5",
+		status: "todo",
+		points: 2,
+	},
+	{
+		title: "Negotiate partnership",
+		id: "BUS-6",
+		status: "todo",
+		points: 4,
+	},
+	{
+		title: "Develop marketing plan",
+		id: "BUS-7",
+		status: "todo",
+		points: 90,
+	},
+	{
+		title: "Evaluate business",
+		id: "BUS-8",
+		status: "in-progress",
+		points: 7,
+	},
+	{
+		title: "Market Research",
+		id: "BUS-9",
+		status: "done",
+		points: 8,
 	},
 ];
