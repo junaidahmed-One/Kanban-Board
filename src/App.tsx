99abad8cd53be074ffbd985735533b673c7b6259
junaidahmed-One <junaidahmed882000@gmail.com>
2024-05-02 12:21:25 +0530

    Task card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
+import { TaskCard } from "./Components/task-card";
 
 function App() {
 	const title = "Hello World";
 	const id = "BUS-1";
 	const points = 5;
 	return (
-		<div className="border rounded-lg px-2 m-2 bg-gray-50">
-			<div className="text-base font-semibold py-2">{title}</div>
-			<div className="flex gap-4 justify-between py-2 text-gray-700">
-				<div>{id}</div>
-				<div>{points}</div>
-			</div>
-		</div>
+		<>
+			<TaskCard title={title} id={id} points={points} />
+			<TaskCard title="Competitior Analysis" id="BUS-2" points={10} />
+			<TaskCard title={title} id={id} points={points} />
+			<TaskCard title={title} id={id} points={points} />
+		</>
 	);
 }
 
