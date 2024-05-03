8e2893a0f2bece897d41a244b91f14249004c275
junaidahmed-One <junaidahmed882000@gmail.com>
2024-05-03 17:21:08 +0530

    drag and drop working without hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import { TaskCard } from "./Components/TaskCard";
-import { tasks as initialTasks, statuses, Task } from "./Types/data-tasks";
+import {
+	tasks as initialTasks,
+	Status,
+	statuses,
+	Task,
+} from "./Types/data-tasks";
 
 function App() {
 	const [tasks, setTasks] = useState<Task[]>(initialTasks);
@@ -13,30 +18,29 @@ function App() {
 		};
 	});
 
-	const updateTaskPoints = (task: Task, points: number) => {
+	const updateTask = (task: Task) => {
 		const updatedTasks = tasks.map((t) => {
-			if (t.id === task.id) {
-				return {
-					...t,
-					points,
-				};
-			}
-			return t;
+			return t.id === task.id ? task : t;
 		});
 		setTasks(updatedTasks);
 	};
 
-	const updateTaskTitle = (task: Task, title: string) => {
-		const updatedTasks = tasks.map((t) => {
-			return t.id === task.id ? { ...t, title } : t;
-		});
-		setTasks(updatedTasks);
+	const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status) => {
+		e.preventDefault();
+		const id = e.dataTransfer.getData("id");
+		const task = tasks.find((task) => task.id === id);
+		if (task) {
+			updateTask({ ...task, status });
+		}
 	};
 
 	return (
 		<div className="flex divide-x">
 			{columns.map((column) => (
-				<div>
+				<div
+					onDrop={(e) => handleDrop(e, column.title)}
+					onDragOver={(e) => e.preventDefault()}
+				>
 					<div className="flex justify-between text-3xl p-2 font-bold">
 						<h2 className="capitalize">{column.title}</h2>
 						{column.tasks.reduce(
@@ -45,11 +49,7 @@ function App() {
 						)}
 					</div>
 					{column.tasks.map((task) => (
-						<TaskCard
-							task={task}
-							updateTaskPoints={updateTaskPoints}
-							updateTaskTitle={updateTaskTitle}
-						/>
+						<TaskCard task={task} updateTask={updateTask} />
 					))}
 				</div>
 			))}
--- src/Components/TaskCard.tsx
+++ src/Components/TaskCard.tsx
@@ -58,21 +58,30 @@ const highPriorityIcon = (
 
 export const TaskCard = ({
 	task,
-	updateTaskPoints,
-	updateTaskTitle,
+	updateTask,
 }: {
 	task: Task;
-	updateTaskPoints: (task: Task, points: number) => void;
-	updateTaskTitle: (task: Task, title: string) => void;
+	updateTask: (task: Task) => void;
 }) => {
 	const [isEditingTitle, setIsEditingTitle] = useState(false);
 	const points = task.points || 0;
-	const updatePoints = (points: number) => {
-		if (points < 0) return;
-		updateTaskPoints(task, points);
+	const updatePoints = (direction: "up" | "down") => {
+		const fib = [0, 1, 2, 3, 5, 8, 13];
+		const index = fib.indexOf(points);
+		const nextIndex = direction === "up" ? index + 1 : index - 1;
+		const newPoints = fib[nextIndex];
+		if (newPoints) {
+			updateTask({ ...task, points: newPoints });
+		}
 	};
 	return (
-		<div className="border rounded-lg px-2 m-2 bg-gray-50 w-56">
+		<div
+			draggable
+			onDragStart={(e) => {
+				e.dataTransfer.setData("id", task.id);
+			}}
+			className="border rounded-lg px-2 m-2 bg-gray-50 w-56"
+		>
 			<div className="text-base font-base py-2">
 				{isEditingTitle ? (
 					<input
@@ -80,7 +89,9 @@ export const TaskCard = ({
 						className="w-full"
 						onBlur={() => setIsEditingTitle(false)}
 						value={task.title}
-						onChange={(e) => updateTaskTitle(task, e.target.value)}
+						onChange={(e) =>
+							updateTask({ ...task, title: e.target.value })
+						}
 					/>
 				) : (
 					<div onClick={() => setIsEditingTitle(true)}>
@@ -96,9 +107,9 @@ export const TaskCard = ({
 					{task.priority === "low" && lowPriorityIcon}
 				</div>
 				<div className="flex gap-2 items-center">
-					<button onClick={() => updatePoints(points - 1)}>-</button>
+					<button onClick={() => updatePoints("down")}>-</button>
 					<div className="font-bold">{points}</div>
-					<button onClick={() => updatePoints(points + 1)}>+</button>
+					<button onClick={() => updatePoints("up")}>+</button>
 				</div>
 			</div>
 		</div>
